1006a1613655a4c7e4ef5e89340c2c3289f885e0
Inu <inayath2410@gmail.com>
2024-05-16 13:28:57 +0530

    Add Prodduct Issue

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Navbar.css'
 import nav_logo from '../../assets/nav-logo.svg'
+import navProfile from '../../assets/nav-profile.svg'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <img src="" alt="" className="nav-logo" />
+      <img src={nav_logo} alt="" className="nav-logo" />
+      <img src={navProfile} alt="" className="nav-profile" />
     </div>
   )
 }
